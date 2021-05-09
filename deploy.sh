f9cc4bad2080887f57c378a455ee7121546e4302
markdferrari <markdferrari@gmail.com>
2021-05-09 11:17:33 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-aws s3 sync . s3://judy-makin.mark-ferrari.com --exclude "*.git*" --exclude "deploy.sh"
+s3_bucket="s3://judy-makin.mark-ferrari.com"
+distribution_id="E1KG8Q9DRF0F74"
+
+# Sync contents to s3
+aws s3 sync . $s3_bucket \
+    --exclude "*.git*" \
+    --exclude "deploy.sh"
+
+echo "Clearing Cloudfront cache"
+# Invalidate cloudfront distribution
+aws cloudfront create-invalidation \
+    --distribution-id $distribution_id \
+    --paths "*"
